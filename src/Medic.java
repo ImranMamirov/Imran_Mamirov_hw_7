9a84661114dbf2a8b5946c4ea63e7f8a8694fca7
ImranMamirov <imranmamirov210806@gmail.com>
2024-02-29 20:01:49 +0700

    Добавил sout в классе Medic

--- src/Medic.java
+++ src/Medic.java
@@ -1,11 +1,13 @@
-public class Medic extends Hero{
+public class Medic extends Hero {
     int healPoints;
+
     @Override
     public void applySuperAbility() {
         System.out.println("Medic применил суперспособность: Healing");
     }
 
-    public void increaseExperience(){
+    public void increaseExperience() {
         healPoints *= 1.1;
+        System.out.println("Medic Увеличил лечения на 10%");
     }
 }
